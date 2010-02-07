f78512cc6619ab1d8671b92ce287ffc8109cba41
hitode909 <hitode909@gmail.com>
2010-02-07 17:32:09 +0900

    db logger

--- models/init.rb
+++ models/init.rb
@@ -1,4 +1,5 @@
 require 'sequel'
+require 'logger'
 
 DB_ENV rescue  DB_ENV = 'app'
 Sequel::Model.plugin(:schema)
@@ -6,7 +7,7 @@ if DB_ENV == 'test'
   DB = Sequel.sqlite('test.db')
 else
   # create database chatlike character set utf8;
-  DB = Sequel.mysql 'eclipse', :user => 'nobody', :password => 'nobody', :host => 'localhost', :encoding => 'utf8'
+  DB = Sequel.mysql 'eclipse', :user => 'nobody', :password => 'nobody', :host => 'localhost', :encoding => 'utf8', :loggers => [Logger.new($stdout)]
 end
 
 require 'models/models'
