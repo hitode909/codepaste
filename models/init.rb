63737b4352ee19ca2ffa6c2d6dbd4d23377de699
hitode909 <hitode909@gmail.com>
2010-02-11 17:52:59 +0900

    changed database name

--- models/init.rb
+++ models/init.rb
@@ -7,7 +7,7 @@ if DB_ENV == 'test'
   DB = Sequel.sqlite('test.db')
 else
   # create database chatlike character set utf8;
-  DB = Sequel.mysql 'eclipse', :user => 'nobody', :password => 'nobody', :host => 'localhost', :encoding => 'utf8', :loggers => [Logger.new($stdout)]
+  DB = Sequel.mysql 'c2', :user => 'nobody', :password => 'nobody', :host => 'localhost', :encoding => 'utf8', :loggers => [Logger.new($stdout)]
 end
 
 require 'models/models'
